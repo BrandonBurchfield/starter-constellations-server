e37883c5f657db7fa518cbf1598f23c03a40df92
Brandon <bburch213@gmail.com>
2021-12-15 12:44:31 -0600

    commit

---
+++ requests-in-javascript/requests.js
@@ -0,0 +1,12 @@
+const { default: axios } = require("axios");
+
+axios
+  .get(constellationsUrl)
+  .then(({ data }) => {
+    const exists = data.find(({ name }) => name === leo.name);
+    if (exists) throw `Constellation "${leo.name}" already exists.`;
+    return axios
+      .post(constellationsUrl, leo)
+      .then(({ data }) => console.log(data));
+  })
+  .catch(console.log);
